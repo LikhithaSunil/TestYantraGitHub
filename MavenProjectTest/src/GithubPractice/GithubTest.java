2cc3d4820d755836e5ae5a86f9dab6f29397aa63
HP <HP@DESKTOP-OSOVURS>
2021-12-11 02:24:26 -0800

    Coding Done

---
+++ MavenProjectTest/src/GithubPractice/GithubTest.java
@@ -0,0 +1,13 @@
+package GithubPractice;
+
+public class GithubTest 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello");
+		System.out.println("hii");
+		System.out.println("bye");
+		
+	}
+
+}
